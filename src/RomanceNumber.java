958a5df50bc5232466a6d9779e752b292997c366
Hieu <Hnguyen22@lamar.edu>
2020-08-19 18:26:26 -0500

    Update Roman Number

--- src/RomanceNumber.java
+++ src/RomanceNumber.java
@@ -1,11 +1,12 @@
 import java.util.HashMap;
-import java.util.Map;
 
-public class TestSolution {
-
-	public static void main(String[] args) {
-		String s = "XXXX";
-
+public class RomanceNumber {   
+	public int romanToInt(String s) {
+		
+		//check to see if the String is empy; 
+		if(s.length() ==0 || s == null) {
+			
+		}
 		HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
 
 		
@@ -15,10 +16,13 @@ public class TestSolution {
 		hm.put('C', 100);
 		hm.put('D', 500);
 		hm.put('M', 1000);
+		//initiate romanTotal with the last value of the String 
 		int romanTotal = hm.get(s.charAt(s.length() - 1));
 
-		
+		//moving from the last index of the String 
 		for(int i= s.length() -2 ; i >=  0; i--) {
+			//because the biggest roman value will be at the beginning of the String
+			//if the left number is smaller than the right number then take the number on he right and subtract the number on the left
 			if(hm.get(s.charAt(i)) >=hm.get(s.charAt(i+1)) ) {
 				romanTotal = romanTotal + hm.get(s.charAt(i));
 			} 
@@ -26,10 +30,8 @@ public class TestSolution {
 			 
 			
 		}
-		System.out.print(romanTotal);
-
-
-
+		
+		return romanTotal;
+		
 	}
-	
 }
