f2ab5fcef46949f554600d5a03faf067a31b35d8
Hieu <Hnguyen22@lamar.edu>
2020-09-03 19:22:21 -0500

    New Update

--- src/ImplementstrStr.java
+++ src/ImplementstrStr.java
@@ -8,18 +8,21 @@ public class ImplementstrStr {
 		System.out.println(strStr(haystack, needle));
 	}
 	public static int strStr(String haystack, String needle) {
-		if(haystack.length() == 0 || needle.length() == 0){
-	        return -1;
-	     }
-		
-		for(int i=0; i < haystack.length(); i ++) {
+
+        if(needle.length() == 0){
+            return 0;
+        }
+        if(haystack.length() == 0 ){
+        return -1;
+       }
+		for(int i=0; i < haystack.length()-(needle.length() -1); i ++) {
 			char hayStackChar = haystack.charAt(i);
-				
+			
 				if(hayStackChar == needle.charAt(0)) {
 					if(needle.length() == 1) {
 						return i;
 					}
-					String subString = (i+needle.length()) > haystack.length()
+			String subString = (i+needle.length()) > haystack.length()
 							?"" : haystack.substring(i, i + needle.length());
 					if(subString.equals(needle)) {
 						return i;
