4a021c40c5b996c793d58289ebd8e4f8373554d2
Hieu Nguyen <hnguyen22@lamar.edu>
2020-09-26 09:17:47 -0500

    Remove Duplicate Number

---
+++ src/RemoveDuplicateNumber.java
@@ -0,0 +1,37 @@
+
+import java.util.Arrays;
+public class RemoveDuplicateNumber {
+
+	public static void main(String[] args) {
+		int[] nums = {
+				0,0,1,1,1,2,2,3,3,4
+		};
+		
+		System.out.println(removeDuplicates(nums));
+
+	}
+	
+	public static int removeDuplicates(int[] nums) { 
+		int result = 0; 
+		
+		int count = 0;
+		int start = 0;
+		Arrays.sort(nums);
+		while (start < nums.length-1) { 
+			start++; 
+			if(nums[count]!= nums[start]) {
+				result += 1; 
+				count = start;
+				
+				nums[result] = nums[count];
+			}
+		}
+		
+		for(int i =0; i <= result; i++) { 
+			System.out.println(nums[i]);
+		}
+		
+		return result+1;
+	}
+
+}
